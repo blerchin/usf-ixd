8a3243906b849a18ff0c6b98bb66e7397f38d87e
Ben Lerchin <ben@lerchin.com>
2018-01-13 11:17:55 -0800

    extract to test/

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -7,7 +7,7 @@ if [ -z "$GITHUB_USERNAME" ] && [ -z "$GITHUB_ACCESS_TOKEN" ]
 then
   echo "Github credentials not present. Skipping autotest."
 else
-  curl -L -u "$GITHUB_USERNAME:$GITHUB_ACCESS_TOKEN" https://api.github.com/repos/usf-ixd/usf-ixd-tests/tarball/master | tar -xz
+  curl -L -u "$GITHUB_USERNAME:$GITHUB_ACCESS_TOKEN" https://api.github.com/repos/usf-ixd/usf-ixd-tests/tarball/master | tar -xzC test
 fi
 
 export NODE_PATH=.
